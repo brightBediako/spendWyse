b74efb59c21a7967686f8f407f10746d51661b05
Bright Bediako <bright.bediako.dev@gmail.com>
2025-10-06 02:34:10 +0000

    configured react query

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from "react-redux";
+import { Query, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import './index.css'
 import App from './App.jsx'
 import { store } from "./redux/store/store.js";
 
 
+// Create a client
+const client = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
+      <QueryClientProvider client={client}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>,
-)
+);
