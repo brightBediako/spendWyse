9bee145794fe21d8f10902790e1b837d7be7befb
Bright Bediako <bright.bediako.dev@gmail.com>
2025-10-07 23:59:21 +0000

    modified files

--- api/config/dbConfig.js
+++ api/config/dbConfig.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const dbConfig = async () => {
-  try {
-    mongoose.set("strictQuery", false);
-    const connected = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`Mongodb connected ${connected.connection.host}`);
-  } catch (error) {
-    console.log(`Error: ${error.message}`);
-    process.exit(1);
-  }
+  // try {
+  //   mongoose.set("strictQuery", false);
+  //   const connected = await mongoose.connect(process.env.MONGO_URI);
+  //   console.log(`Mongodb connected ${connected.connection.host}`);
+  // } catch (error) {
+  //   console.log(`Error: ${error.message}`);
+  //   process.exit(1);
+  // }
 };
 
 export default dbConfig;
